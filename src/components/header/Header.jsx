08efa4dd48f8738715c2d09263f6a9080934394f
Luca Ragazzini <l.ragazzini@bipres.it>
2024-07-23 17:25:23 +0200

    upload color set with original bipres color

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,14 +4,14 @@ export const Header = () => {
   const isSmallScreen = window.innerWidth <= 768; // Aggiungi questa variabile per controllare la dimensione dello schermo
 
   return (
-    <div style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
+    <div style={{ width: "100vw", height: "auto", overflow: "hidden" }}>
       {isSmallScreen ? (
         <video
           src={Corporate}
           autoPlay
           loop
           muted
-          style={{ width: "100%", height: "900px", objectFit: "cover" }}
+          style={{ width: "100%", height: "400px", objectFit: "cover" }}
         />
       ) : (
         <video
