33d703ce687a90d07f1565ee05d0a39e7a7f2bb1
Luca Ragazzini <80540642+rgzzn@users.noreply.github.com>
2024-08-06 17:15:28 +0000

    motto jsx docs specs

--- src/components/motto/Motto.jsx
+++ src/components/motto/Motto.jsx
@@ -1,3 +1,7 @@
+/**
+ * Renders the Motto component.
+ * @returns {JSX.Element} The rendered Motto component.
+ */
 export const Motto = () => {
   return (
     <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-10">
