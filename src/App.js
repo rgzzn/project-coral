d4317d89e4360d913ae159a8fbda6d04ea76960c
Luca Ragazzini <rgzzn@hrz.me>
2026-02-25 17:18:19 +0100

    Ottimizza build Vercel e snellisce dipendenze npm

--- src/App.js
+++ src/App.js
@@ -25,237 +25,44 @@ import LavorazioniCNC from "./pages/macchine/lavorazionicnc";
 import StampaggioLAM from "./pages/macchine/stampaggiolam";
 import Referenze from "./pages/referenze";
 
+const routes = [
+  { path: "/", element: <Homepage /> },
+  { path: "/settori", element: <Settori /> },
+  { path: "/settori/elettrodomestico", element: <Elettrodomestico /> },
+  { path: "/settori/caffe", element: <Caffe /> },
+  { path: "/settori/arredamento", element: <Arredamento /> },
+  { path: "/settori/serrature", element: <Serrature /> },
+  { path: "/settori/ceramico", element: <Ceramico /> },
+  { path: "/settori/packaging", element: <Packaging /> },
+  { path: "/contact", element: <Contact /> },
+  { path: "/lavorazioni", element: <Lavorazioni /> },
+  { path: "/lavorazioni/assemblaggio", element: <Assemblaggio /> },
+  { path: "/lavorazioni/cnc", element: <CNC /> },
+  { path: "/lavorazioni/carpenteria", element: <Carpenteria /> },
+  { path: "/lavorazioni/stampaggio", element: <Stampaggio /> },
+  { path: "/knowhow", element: <KnowHow /> },
+  { path: "/knowhow/commessa", element: <Commessa /> },
+  { path: "/knowhow/prototipazione", element: <Prototipazione /> },
+  { path: "/knowhow/fattibilita", element: <Fattibilita /> },
+  { path: "/macchine", element: <ParcoMacchine /> },
+  { path: "/macchine/metrologica", element: <Metrologica /> },
+  { path: "/macchine/lavorazionicnc", element: <LavorazioniCNC /> },
+  { path: "/macchine/stampaggiolam", element: <StampaggioLAM /> },
+  { path: "/referenze", element: <Referenze /> },
+];
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/settori" element={<SettoriPage />} />
-          <Route
-            path="/settori/elettrodomestico"
-            element={<ElettrodomesticoPage />}
-          />
-          <Route path="/settori/caffe" element={<CaffePage />} />
-          <Route path="/settori/arredamento" element={<ArredamentoPage />} />
-          <Route path="/settori/serrature" element={<SerraturePage />} />
-          <Route path="/settori/ceramico" element={<CeramicoPage />} />
-          <Route path="/settori/packaging" element={<PackagingPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/lavorazioni" element={<LavorazioniPage />} />
-          <Route
-            path="/lavorazioni/assemblaggio"
-            element={<AssemblaggioPage />}
-          />
-          <Route path="/lavorazioni/cnc" element={<CNCPage />} />\
-          <Route
-            path="/lavorazioni/carpenteria"
-            element={<CarpenteriaPage />}
-          />
-          <Route path="/lavorazioni/stampaggio" element={<StampaggioPage />} />
-          <Route path="/knowhow" element={<KnowHowPage />} />
-          <Route path="/knowhow/commessa" element={<CommessaPage />} />
-          <Route
-            path="/knowhow/prototipazione"
-            element={<PrototipazionePage />}
-          />
-          <Route path="/knowhow/fattibilita" element={<FattibilitaPage />} />
-          <Route path="/macchine" element={<ParcoMacchinePage />} />
-          <Route path="/macchine/metrologica" element={<MetrologicaPage />} />
-          <Route
-            path="/macchine/lavorazionicnc"
-            element={<LavorazioniCNCPage />}
-          />
-          <Route path="/macchine/stampaggiolam" element={<StampaggioLAMPage />}/>
-          <Route path="/referenze" element={<ReferenzePage/>}/>
-          <Route path="/casehistory" element={<caseHistoryPage/>}/>
+          {routes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
         </Routes>
       </div>
     </BrowserRouter>
   );
 }
 
-function Home() {
-  return (
-    <div>
-      <Homepage />
-    </div>
-  );
-}
-
-function SettoriPage() {
-  return (
-    <div>
-      <Settori />
-    </div>
-  );
-}
-
-function ElettrodomesticoPage() {
-  return (
-    <div>
-      <Elettrodomestico />
-    </div>
-  );
-}
-
-function CaffePage() {
-  return (
-    <div>
-      <Caffe />
-    </div>
-  );
-}
-function ArredamentoPage() {
-  return (
-    <div>
-      <Arredamento />
-    </div>
-  );
-}
-
-function SerraturePage() {
-  return (
-    <div>
-      <Serrature />
-    </div>
-  );
-}
-
-function CeramicoPage() {
-  return (
-    <div>
-      <Ceramico />
-    </div>
-  );
-}
-
-function PackagingPage() {
-  return (
-    <div>
-      <Packaging />
-    </div>
-  );
-}
-
-function ContactPage() {
-  return (
-    <div>
-      <Contact />
-    </div>
-  );
-}
-
-function LavorazioniPage() {
-  return (
-    <div>
-      <Lavorazioni />
-    </div>
-  );
-}
-
-function AssemblaggioPage() {
-  return (
-    <div>
-      <Assemblaggio />
-    </div>
-  );
-}
-
-function CNCPage() {
-  return (
-    <div>
-      <CNC />
-    </div>
-  );
-}
-
-function CarpenteriaPage() {
-  return (
-    <div>
-      <Carpenteria />
-    </div>
-  );
-}
-
-function StampaggioPage() {
-  return (
-    <div>
-      <Stampaggio />
-    </div>
-  );
-}
-
-function KnowHowPage() {
-  return (
-    <div>
-      <KnowHow />
-    </div>
-  );
-}
-
-function CommessaPage() {
-  return (
-    <div>
-      <Commessa />
-    </div>
-  );
-}
-
-function PrototipazionePage() {
-  return (
-    <div>
-      <Prototipazione />
-    </div>
-  );
-}
-
-function FattibilitaPage() {
-  return (
-    <div>
-      <Fattibilita />
-    </div>
-  );
-}
-
-function ParcoMacchinePage() {
-  return (
-    <div>
-      <ParcoMacchine />
-    </div>
-  );
-}
-
-function MetrologicaPage() {
-  return (
-    <div>
-      <Metrologica />
-    </div>
-  );
-}
-
-function LavorazioniCNCPage() {
-  return (
-    <div>
-      <LavorazioniCNC />
-    </div>
-  );
-}
-
-function StampaggioLAMPage() {
-  return (
-    <div>
-      <StampaggioLAM />
-    </div>
-  )
-}
-
-function ReferenzePage() {
-  return (
-    <div>
-      <Referenze />
-    </div>
-  )
-}
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders main navigation links', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getAllByRole('link', { name: /home/i }).length).toBeGreaterThan(0);
+  expect(screen.getAllByRole('link', { name: /settori/i }).length).toBeGreaterThan(0);
 });
